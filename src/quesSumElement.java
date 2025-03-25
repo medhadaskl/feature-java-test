4f4c581cee71edc0916d324c94c44a0c9c8b2e16
medhadaskl <medha.das@kibalabs.in>
2025-03-25 16:35:27 +0530

    Initial commit

---
+++ src/quesSumElement.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class quesSumElement {
+    public static void main(String[] args) {
+        int arr[]={ 2, 4, 6, 7, 9};
+        int sum = 0;
+        for(int i=0;i<=arr.length;i++){
+            sum= arr[i]+arr[i++];
+        }
+        System.out.print("Sum of array elements : "+sum);
+
+    }
+}
